06983d18af12ef8150a77999c491723ca1b2649e
Ilya Timofeev <il.tmfv@gmail.com>
2018-05-22 23:48:34 +0300

    Remove unused key from project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  ;:jvm-opts ["--add-modules" "java.xml.bind"]
   :dependencies [
                  [org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
